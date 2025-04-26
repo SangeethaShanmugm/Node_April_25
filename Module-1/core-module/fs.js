50ac8ba7df7268e88b975d9b19d6e0282334defc
SangeethaShanmugm <sanjuriya22@gmail.com>
2025-04-26 23:38:45 +0530

    updated

---
+++ Module-1/core-module/fs.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+// console.log("Hello Everyone🥳");
+
+//create a new file
+
+const quote = "Make everyday a little less ordinarily🥳🥳"
+
+//callback function => function inside an another function(nested func)
+
+// fs.writeFile("fun.pdf", quote, () => {
+//     console.log("File written successfully")
+// })
+
+const quote2 = "Live more, worry less"
+
+//backup => text-1.html, text-2.html...text-10.html
+//`` => backtick, ${} => interpolation => substitute the value
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+for (let i = 1; i <= 10; i++) {
+    fs.writeFile(`./backup/text-${i}.html`, quote, () => {
+        console.log(`File written successfully for text-${i}.html`);
+    })
+}
